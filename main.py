db31433efb6079a2b2bccead2df92674a0bbf9c2
intikamailesi <99623476+intikamailesi@users.noreply.github.com>
2022-03-11 11:40:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,6 @@ from core.player import player
 from os import path, mkdir
 
 
-def main():
-    if not path.exists("search"):
-        mkdir("search")
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(player.run())
 
-
+player.run()
 main()
